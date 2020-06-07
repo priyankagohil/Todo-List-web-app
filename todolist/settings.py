3cdfe16e0bef83064c3859a20be0e2e684f92542
Priyanka Gohil <priyankapgohil@gmail.com>
2020-06-07 16:48:05 +0530

    new local settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/login'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production.")
